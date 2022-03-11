bd0e6087aef4ee85e89ed38a5b54f1ee85586b6a
Pankaj Nikam <pankaj-nikam@users.noreply.github.com>
2022-03-11 07:24:10 +0530

    Test.

--- src/main.rs
+++ src/main.rs
@@ -9,6 +9,6 @@ async fn main() -> std::io::Result<()> {
     let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
     // We retrieve the port assigned to us by the OS
     let port = listener.local_addr().unwrap().port();
-    println!("Running on port {}", port);
+    println!("Running on  port {}", port);
     run(listener)?.await
 }
